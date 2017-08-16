5d13968dc5f83632c5360f4a96d6d6b0d106286a
kagikn <kagikn.3939@gmail.com>
2017-08-16 09:24:04 +0900

    Add copyright info to Assembly

--- StuntBonusV/Properties/AssemblyInfo.cs
+++ StuntBonusV/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("StuntBonusV")]
+[assembly: AssemblyTitle("Stunt Bonus V")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StuntBonusV")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Stunt Bonus V")]
+[assembly: AssemblyCopyright("Copyright © 2017 kagikn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
