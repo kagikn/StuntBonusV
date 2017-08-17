b17b9958e1fab5391e9b7daffdcb60c250ad142d
kagikn <kagikn.3939@gmail.com>
2017-08-17 14:52:10 +0900

    Add InsaneStuntBonusResult

---
+++ StuntBonusV/InsaneStuntBonusResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+
+namespace StuntBonusV
+{
+    internal class InsaneStuntBonusResult
+    {
+        private Vehicle Vehicle { get; }
+        private float DistanceXY { get; }
+        private float StuntHeight { get; }
+        private int FlipCount { get; }
+        private float TotalHeadingRotation { get; }
+        private float VehicleBodyHealth { get; }
+        private float VehicleEngineHealth { get; }
+        private float VehicleFuelTankHealth { get; }
+
+        public InsaneStuntBonusResult(Vehicle vehicle, float distanceXY, float stuntHeight, int flipCount, int totalHeadingRotation)
+        {
+            if (!vehicle.ExistsSafe())
+            {
+                throw new ArgumentNullException("vehicle");
+            }
+
+            Vehicle = vehicle;
+            DistanceXY = distanceXY;
+            StuntHeight = stuntHeight;
+            FlipCount = flipCount;
+            TotalHeadingRotation = totalHeadingRotation;
+
+            VehicleBodyHealth = vehicle.BodyHealth;
+            VehicleEngineHealth = vehicle.EngineHealth;
+            VehicleFuelTankHealth = vehicle.PetrolTankHealth;
+        }
+    }
+}
